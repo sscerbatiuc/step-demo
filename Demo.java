7cf94a84c450c2a03ac67f8e3bbcf9bc91d63eba
sscerbatiuc <s.scerbatiuc@jdi.nl>
2023-04-11 20:57:52 +0300

    Added demo file

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Test {
+	public static void main(String[] args) {
+		
+		int x = -10;
+		if(x > 0) {
+			System.out.println("Acest numar este mai mare decat 0");
+			System.out.println("--- Executam logica ---");
+			System.out.println("--- New lines ---");
+		} else if(x > -200) {
+			System.out.println("Acest numar este mai mare decat -200");
+		} else {
+			System.out.println("Acest numar este mai mic decat -200");
+		}
+	}
+}
